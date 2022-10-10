35aa9f82ce09185fbd9dcea90533008930119e91
nik1097 <nikhilsuresh097@gmail.com>
2022-10-09 23:26:15 -0400

    assignment 2 completed

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // use the express library
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 // create a new server application
 const app = express();
@@ -10,14 +11,48 @@ const app = express();
 // world wide web).
 const port = process.env.PORT || 3000;
 
-
-// The main page of our website
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+//To allow sever to use public folder
+app.use(express.static('public'));
 
 // Start listening for network connections
 app.listen(port);
 
 // Printout for readability
 console.log("Server Started!");
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//Cookie Parser
+app.use(cookieParser());
+
+let nextVisitorId = 1;
+app.get('/', (req, res) => {
+  let lastVisit = req.cookies.visited;
+  let visitorId = req.cookies.visitorId;
+
+  if (lastVisit) {
+    lastVisit = (parseInt((Date.now() - req.cookies.visited)/1000));
+    msg = "It has been " + lastVisit+" seconds since your last visit!";
+  }
+  else
+    msg = "You have never visited before.";
+
+  console.log(visitorId)
+  if (visitorId === undefined){
+    visitorId =  ++nextVisitorId;
+    res.cookie('visitorId', visitorId);
+  }
+  else
+    visitorId =  req.cookies.visitorId;
+
+  res.cookie('visitorId', visitorId);
+  res.cookie('visited', Date.now().toString());
+  res.render('welcome', {
+        name: req.query.name || "World",
+        date: new Date().toLocaleString(),
+        lastVisit : lastVisit ,
+        visitorId : visitorId,
+        msg: msg
+  });
+}); 
